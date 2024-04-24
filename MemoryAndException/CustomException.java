d7b29923ece634f8f24afc4505ca3fecb8fa4f4e
HiddenGrizzly <153844656+BrownyGrizzly@users.noreply.github.com>
2024-04-24 14:14:02 +0700

    Change

---
+++ MemoryAndException/CustomException.java
@@ -0,0 +1,8 @@
+package MemoryAndException;
+
+class CustomException extends Exception {
+    // Constructor with a message
+    public CustomException(String message) {
+        super(message);
+    }
+}
--- MemoryAndException/ThrowsDemo.java
+++ MemoryAndException/ThrowsDemo.java
@@ -1,20 +1,20 @@
 package MemoryAndException;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 public class ThrowsDemo {
-    public static void main(String[] args) {
-        try {
-            readFile("file.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + e.getMessage());
+    // Method that throws custom exception
+    public static void validateAge(int age) throws CustomException {
+        if (age < 0) {
+            throw new CustomException("Tuổi không âm.");
         }
     }
 
-    public static void readFile(String fileName) throws FileNotFoundException {
-        FileInputStream fis = new FileInputStream(fileName);
-        // Read file...
+    public static void main(String[] args) {
+        int age = -1;
+        try {
+            validateAge(age);
+        } catch (CustomException e) {
+            System.out.println("Xảy ra ngoại lệ: " + e.getMessage());
+        }
     }
 }
 
